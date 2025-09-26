5ce00f60b69000d6b920cc98849d371037dcdd8b
Steven Yackel <styackel@microsoft.com>
2025-09-26 14:15:09 -0500

    adds base dependency injection

---
+++ Lectures/09-30-2025 Dependency Injection/Hobbits/Controllers/HobbitsController.cs
@@ -0,0 +1,42 @@
+﻿using Hobbits.Entities;
+using Hobbits.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace Hobbits.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HobbitsController : Controller
+    {
+        private static readonly HobbitsDatabase hobbitsDatabase = new();
+
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var hobbits = hobbitsDatabase.GetAll().Select(hobbit => new HobbitEntity(hobbit));
+
+            return Json(hobbits);
+        }
+
+        [HttpGet("{index:int}")]
+        public IActionResult Get(int index)
+        {
+            if (index < 0 || index >= hobbitsDatabase.Count)
+            {
+                return StatusCode((int)HttpStatusCode.NotFound);
+            }
+
+            return Json(new HobbitEntity(hobbitsDatabase.Get(index)));
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] HobbitEntity hobbitEntity)
+        {
+            hobbitsDatabase.Add(hobbitEntity.ToModel());
+
+            return Json(hobbitEntity);
+        }
+    }
+}
