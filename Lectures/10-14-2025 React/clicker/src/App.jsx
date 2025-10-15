570d438476115996e3527712f626e11c1762607b
Steven Yackel <spazard1@gmail.com>
2025-10-14 20:19:24 -0500

    react lecture

--- Lectures/10-14-2025 React/clicker/src/App.jsx
+++ Lectures/10-14-2025 React/clicker/src/App.jsx
@@ -1,13 +1,40 @@
 import ClickCounter from "./components/ClickCounter";
+import { useCallback, useEffect, useState } from "react";
 
 import "./App.css";
 
 const App = () => {
+  const [teamScores, setTeamScores] = useState({ "Team 1": 0, "Team 2": 0 });
+
+  const onScoreChange = useCallback(
+    (teamName, score) => {
+      setTeamScores({ ...teamScores, [teamName]: score });
+    },
+    [teamScores]
+  );
+
+  const updateDocumentTitle = useCallback(() => {
+    let newTitle = "";
+    Object.keys(teamScores).map((teamName) => {
+      newTitle += teamName + ":" + teamScores[teamName] + " ";
+    });
+    document.title = newTitle;
+  }, [teamScores]);
+
+  useEffect(() => {
+    updateDocumentTitle();
+  }, [teamScores, updateDocumentTitle]);
+
   return (
     <div className={"appContainer"}>
-      <ClickCounter teamName="Team 1"></ClickCounter>
-      <ClickCounter teamName="Team 2"></ClickCounter>
-      <ClickCounter teamName="Team 3" startingScore={5}></ClickCounter>
+      {Object.keys(teamScores).map((teamName) => (
+        <ClickCounter
+          key={teamName}
+          teamName={teamName}
+          scoreValue={teamScores[teamName] ?? 0}
+          onScoreChange={onScoreChange}
+        ></ClickCounter>
+      ))}
     </div>
   );
 };
--- Lectures/10-14-2025 React/clicker/src/components/ClickCounter.jsx
+++ Lectures/10-14-2025 React/clicker/src/components/ClickCounter.jsx
@@ -1,17 +1,18 @@
-import { useState } from "react";
 import { Button } from "react-bootstrap";
 import "./ClickCounter.css";
 
-const ClickCounter = ({ teamName, startingScore }) => {
-  const [scoreValue, setScoreValue] = useState(startingScore ?? 0);
-
+const ClickCounter = ({ teamName, scoreValue, onScoreChange }) => {
   return (
     <div className="clickCounterContainer">
       <div className="teamName">{teamName}</div>
       <div className="buttonsContainer">
-        <Button onClick={() => setScoreValue(scoreValue + 1)}>Increment</Button>
+        <Button onClick={() => onScoreChange(teamName, scoreValue + 1)}>
+          Increment
+        </Button>
         <div className="score">{scoreValue}</div>
-        <Button onClick={() => setScoreValue(scoreValue - 1)}>Decrement</Button>
+        <Button onClick={() => onScoreChange(teamName, scoreValue - 1)}>
+          Decrement
+        </Button>
       </div>
     </div>
   );
