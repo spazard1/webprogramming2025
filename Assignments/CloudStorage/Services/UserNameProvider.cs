9fda58a9555679b173ec74e6e494384590ebb6eb
Steven Yackel <styackel@microsoft.com>
2025-10-28 10:48:50 -0500

    fix typo

--- Assignments/CloudStorage/Services/UserNameProvider.cs
+++ Assignments/CloudStorage/Services/UserNameProvider.cs
@@ -4,7 +4,7 @@
     {
         /*
          * Note: this is not the same username as your microsoft account. This is your username without any upper case letters or special characters.
-         * For example: 'dragon.keeeper@microsoft.com' would become 'dragonkeeper' here.
+         * For example: 'dragon.keeper@microsoft.com' would become 'dragonkeeper' here.
          */
         public string UserName => throw new InvalidDataException("Replace this exception with your username.");
     }
